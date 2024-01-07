a732b997604cb854d946d4af9283251007365718
Sophhdalee <sophiewillis963@gmail.com>
2024-01-07 17:39:38 +0000

    update form 1
    Commented code for form1

--- ASE Assignment/Form1.cs
+++ ASE Assignment/Form1.cs
@@ -4,29 +4,37 @@ using System.Windows.Forms;
 
 namespace ASE_Assignment
 {
+    // Define the Form1 class, inheriting from the Form class
     public partial class Form1 : Form
     {
+        // Initialize a Bitmap object for drawing with a size off 500x300
         Bitmap imageoutput = new Bitmap(500, 300);
+        // declares shapes objects for handling drawing operations
         Shapes shapes;
+        // if shapes should be filled or not 
         bool shapeFill = false;
 
 
-
+        // Constructor for Form1
         public Form1()
         {
             InitializeComponent();
+            // Initialize the Shapes object with the graphics context from the Bitmap
             shapes = new Shapes(Graphics.FromImage(imageoutput));
+            // show in textbox if shape fill is off
             textboxToShowFill.Text = "Shape fill off";
 
         }
 
-
+        // Event handler for the KeyDown event of the commandLine textbox
         private void commandLine_KeyDown(object sender, KeyEventArgs e)
         {
 
-
+         // Check if the Enter key is pressed
             if (e.KeyCode == Keys.Enter)
             {
+
+         // Clear any previous syntax error messages
                 textboxForSyntaxErrors.Text = "";
                 string userInput = inputTextbox.Text.ToLower().Trim();
                 string longUserInput = longInputTextbox.Text;
@@ -36,6 +44,7 @@ namespace ASE_Assignment
                         case "reset":
                             shapes.resetCanvas();
                             break;
+                    // handles pen commands
                         case "red pen":
                             shapes.redPen();
                             break;
@@ -54,6 +63,7 @@ namespace ASE_Assignment
                         case "clear":
                             shapes.clearCanvas();
                             break;
+                    // handles fill on and off commands
                         case "fill on":
                             shapeFill = true;
                             textboxToShowFill.Text = "Fill shape on";
@@ -62,6 +72,7 @@ namespace ASE_Assignment
                             shapeFill = false;
                             textboxToShowFill.Text = "Fill shape off";
                             break;
+                    // error messages for drawing 
                         case "drwato":
                             textboxForSyntaxErrors.Text = "Incorrect number of params please input 2 numbers";
                             break;
@@ -78,10 +89,12 @@ namespace ASE_Assignment
                             textboxForSyntaxErrors.Text = "Incorrect number of params please input numbers";
                             break;
                     case "run":
-                        LongInput.LongInputCommand(longUserInput, shapes, shapeFill, textboxForSyntaxErrors);
+                    // Call a method to process a long input command
+                    LongInput.LongInputCommand(longUserInput, shapes, shapeFill, textboxForSyntaxErrors);
                         break;
                     default:
-                            Parser.ParseCommand(userInput, shapes, shapeFill, textboxForSyntaxErrors);
+                    // Call a general parser to process commands
+                    Parser.ParseCommand(userInput, shapes, shapeFill, textboxForSyntaxErrors);
                             break;
                     }
                 
@@ -89,26 +102,27 @@ namespace ASE_Assignment
                 Refresh();
             }
         }
-
+        // Event handler for the Paint event of the outputCanvas
         private void outputCanvas_Paint(object sender, PaintEventArgs e)
         {
             Graphics graphics = e.Graphics;
             graphics.DrawImageUnscaled(imageoutput, 0, 0);
         }
 
-
+        // Event handler for the MouseClick event of the commandLine textbox
         private void commandLine_MouseClick(object sender, MouseEventArgs e)
         {
+            // Clear the input textbox when clicked
             inputTextbox.Text = "";
         }
 
-
+        // Event handler for the Click event of the buttonToLoad button
         private void buttonToLoad_Click(object sender, EventArgs e)
         {
             LoadLongInput.LoadLongCommands(longInputTextbox);
         }
 
-
+        // Event handler for the Click event of the buttonToSave button
         private void buttonToSave_Click(object sender, EventArgs e)
         {
             SaveLongInput.SaveLongCommands(longInputTextbox);
